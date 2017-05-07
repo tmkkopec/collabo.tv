ef45aed4bbc24ba3b106c50f3767b272db5f5b33
ksurulo <krzysztof.surulo@gmail.com>
2017-05-07 21:48:24 +0200

    Prototype of User Cameras Streams

--- index.js
+++ index.js
@@ -40,6 +40,44 @@ app.get('/debug', function(req, res) {
     res.send(req.cookies);
 });
 
-app.listen(port, function() {
+var server= app.listen(port, function() {
     console.log("App started on port 3000");
 });
+var io = require('socket.io').listen(server),
+connections = {};
+
+io.on('connection', function (socket) {
+
+    socket.on('create or join', function (room) {
+        var rooms = io.sockets.adapter.rooms;
+
+        // create a room if it doesn't exist
+        if (!(room in rooms)) {
+            socket.join(room);
+            socket.emit('created', room);
+            console.log('created room', room);
+        } else {
+            // join the room if it's not full
+            var numClients = Object.keys(rooms[room]).length;
+            if (numClients == 1) {
+                socket.join(room);
+
+                // notify yourself and others in the room
+                socket.emit('joined', room);
+                socket.to(room).emit('join', room);
+
+                console.log('joined room', room);
+            } else { // max two clients
+                console.log('room', room, 'is full');
+            }
+        }
+    });
+
+    socket.on('message', function (room, message) {
+        console.log(room, message);
+        socket.to(room).emit('message', message);
+    });
+
+});
+
+module.exports = io;
