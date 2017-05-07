e3cb964320129b5221b305717a0b6b0bd392315c
ksurulo <krzysztof.surulo@gmail.com>
2017-05-07 22:51:28 +0200

    I will clean up later

--- index.js
+++ index.js
@@ -13,71 +13,87 @@ app.use(cookieParser());
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'views', 'login.html'));
 });
 
-app.get('/join', function(req, res) {
+app.get('/join', function (req, res) {
     var query = req.query.nickname.split('@');
-    res.cookie('nickname', query[0], { expires: new Date(Date.now() + 60000) });
+    res.cookie('nickname', query[0], {expires: new Date(Date.now() + 60000)});
     res.redirect('/room/' + query[1]);
 });
 
-app.get('/room/:id', function(req, res) {
+app.get('/room/:id', function (req, res) {
     if (req.cookies.nickname)
         res.sendFile(path.join(__dirname, 'public', 'views', 'room.html'));
     else
         res.redirect('/');
 });
 
-app.get('/room', function(req, res) {
+app.get('/room', function (req, res) {
     res.redirect('/');
 })
 
-app.get('/debug', function(req, res) {
+app.get('/debug', function (req, res) {
     res.clearCookie('nickname');
     res.set('Content-Type', 'text/html');
     res.send(req.cookies);
 });
 
-var server= app.listen(port, function() {
+var server = app.listen(port, function () {
     console.log("App started on port 3000");
 });
-var io = require('socket.io').listen(server),
-connections = {};
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function(socket) {
 
-io.on('connection', function (socket) {
+    // convenience function to log server messages on the client
+    function log() {
+        var array = ['Message from server:'];
+        array.push.apply(array, arguments);
+        socket.emit('log', array);
+    }
 
-    socket.on('create or join', function (room) {
-        var rooms = io.sockets.adapter.rooms;
+    socket.on('message', function(message) {
+        log('Client said: ', message);
+        // for a real app, would be room-only (not broadcast)
+        socket.broadcast.emit('message', message);
+    });
 
-        // create a room if it doesn't exist
-        if (!(room in rooms)) {
+    socket.on('create or join', function(room) {
+        log('Received request to create or join room ' + room);
+
+        var numClients = io.sockets.sockets.length;
+        log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+        if (numClients === 1) {
             socket.join(room);
-            socket.emit('created', room);
-            console.log('created room', room);
-        } else {
-            // join the room if it's not full
-            var numClients = Object.keys(rooms[room]).length;
-            if (numClients == 1) {
-                socket.join(room);
+            log('Client ID ' + socket.id + ' created room ' + room);
+            socket.emit('created', room, socket.id);
 
-                // notify yourself and others in the room
-                socket.emit('joined', room);
-                socket.to(room).emit('join', room);
-
-                console.log('joined room', room);
-            } else { // max two clients
-                console.log('room', room, 'is full');
-            }
+        } else if (numClients === 2) {
+            log('Client ID ' + socket.id + ' joined room ' + room);
+            io.sockets.in(room).emit('join', room);
+            socket.join(room);
+            socket.emit('joined', room, socket.id);
+            io.sockets.in(room).emit('ready');
+        } else { // max two clients
+            socket.emit('full', room);
         }
     });
 
-    socket.on('message', function (room, message) {
-        console.log(room, message);
-        socket.to(room).emit('message', message);
+    socket.on('ipaddr', function() {
+        var ifaces = os.networkInterfaces();
+        for (var dev in ifaces) {
+            ifaces[dev].forEach(function(details) {
+                if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+                    socket.emit('ipaddr', details.address);
+                }
+            });
+        }
+    });
+
+    socket.on('bye', function(){
+        console.log('received bye');
     });
 
 });
-
-module.exports = io;
